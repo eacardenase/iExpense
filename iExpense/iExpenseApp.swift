a282091f387d456606f018c76c58a0715ab1e276
Edwin Cardenas <eacardenase@gmail.com>
2026-04-20 16:51:52 -0500

    Initial Commit

---
+++ iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by Edwin Cardenas on 4/20/26.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
