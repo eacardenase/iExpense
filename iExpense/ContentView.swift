0b8ab226c500eca3ef49ddb39c0ec86ed0d70f14
Edwin Cardenas <eacardenase@gmail.com>
2026-04-21 13:37:31 -0500

    refactor: use @AppStorage wrapper instead of directly UserDefaults

--- iExpense/ContentView.swift
+++ iExpense/ContentView.swift
@@ -8,22 +8,11 @@
 import SwiftUI
 
 struct ContentView: View {
-    // @State private var tapCount = UserDefaults.standard.integer(forKey: "Tap")
-    @State private var tapCount: Int = {
-        guard
-            let count = UserDefaults.standard.value(forKey: "Tap") as? Int
-        else {
-            return 0
-        }
-
-        return count
-    }()
+    @AppStorage("tapCount") private var tapCount = 0
 
     var body: some View {
         Button("Tap count: \(tapCount)") {
             tapCount += 1
-
-            UserDefaults.standard.set(tapCount, forKey: "Tap")
         }
     }
 }
