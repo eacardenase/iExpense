a4da924e8cd1a1ab552ae07c8500ebbb6e709b7c
Edwin Cardenas <eacardenase@gmail.com>
2026-04-21 14:09:54 -0500

    feat: store custom types on UserDefaults

--- iExpense/ContentView.swift
+++ iExpense/ContentView.swift
@@ -7,12 +7,47 @@
 
 import SwiftUI
 
+struct User: Codable {
+    var firstName = ""
+    var lastName = ""
+}
+
 struct ContentView: View {
-    @AppStorage("tapCount") private var tapCount = 0
+    @State private var user: User = {
+        guard
+            let userData = UserDefaults.standard.data(forKey: "UserData")
+        else { return User() }
+
+        let decoder = JSONDecoder()
+        if let user = try? decoder.decode(User.self, from: userData) {
+            return user
+        }
+
+        return User()
+    }()
 
     var body: some View {
-        Button("Tap count: \(tapCount)") {
-            tapCount += 1
+        NavigationStack {
+            List {
+                Text("Your name is: \(user.firstName) \(user.lastName)")
+
+                Section {
+                    TextField("First name", text: $user.firstName)
+                    TextField("Last name", text: $user.lastName)
+                }
+            }
+            .navigationTitle("Me")
+            .toolbar {
+                Button {
+                    let encoder = JSONEncoder()
+
+                    if let data = try? encoder.encode(user) {
+                        UserDefaults.standard.set(data, forKey: "UserData")
+                    }
+                } label: {
+                    Label("Save", systemImage: "square.and.arrow.down")
+                }
+            }
         }
     }
 }
